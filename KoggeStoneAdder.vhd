303ddefcd0318165f70e17f5423ff52134f1b58a
f.paetzke <f.paetzke@googlemail.com>
2011-07-01 10:30:54 +0200

    KoggeStone^^

--- KoggeStoneAdder.vhd
+++ KoggeStoneAdder.vhd
@@ -7,14 +7,12 @@ entity KoggeStoneAdder is
 		P: in std_logic_vector;
 		G: in std_logic_vector;
 		cin: in std_logic;
+		sum: out std_logic_vector;
 		cout: out std_logic
 	);
 end KoggeStoneAdder;
 
 architecture Behavioral of KoggeStoneAdder is
-	--variable dist: integer;
-
-
 	signal len: natural := log2(P'length + 1);
 	signal dist: integer;
 	
@@ -36,15 +34,15 @@ forGen:		for i in tG'high to 1 generate
 stGen0:		for i in 1 to (len - 1) generate
 stGen1:			for j in 0 to tG'high generate
 	
-						--dist <= j - i ** 2;
-						
-						if (j < 2 ** i) then
+						dist <= j - 2 ** i;
+					
+g1:					if (dist < 0) generate
 							tG(i)(j) <= tG(i - 1)(j);
 							tP(i)(j) <= tP(i - 1)(j);
-						end if;			
+						end generate;			
 						
 						
-ifGen:				if (j >= 2 ** i) generate
+ifGen:				if (dist >= 0) generate
 prfxInst:				Prefix port map(tG(i - 1)(j), tP(i - 1)(j),  
 														  tG(i - 1)(j - dist), tP(i - 1)(j - dist),
 													     tG(i)(j), tP(i)(j));	
@@ -53,6 +51,10 @@ prfxInst:				Prefix port map(tG(i - 1)(j), tP(i - 1)(j),
 					end generate;
 				end generate;
 	
+g2:			for i in 0 to tg'high - 1 generate
+					sum(i + sum'low) <= tG(i);
+				end generate;
+				cout <= tG(tG'high);
 
 
 end Behavioral;
